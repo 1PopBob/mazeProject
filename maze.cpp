882ed071deade18ad30f854a5c510af1346797a1
Joshua Arvizu <arvizu.m.joshua@gmail.com>
2022-08-03 15:35:31 -0700

    Updated comments

--- maze.cpp
+++ maze.cpp
@@ -8,13 +8,11 @@ maze.cpp
 
 using namespace std;
 
-// Prototype for maze_search, which you will fill in below.
 int maze_search(char**, int, int);
 bool checkDuplicate(Location, const int*, Location*);
 bool addToExplored(Location, int*, Location*);
 void backtracking(Location**, char**, Location, int);
 
-// main function to read, solve maze, and print result
 int main(int argc, char* argv[]) {
     int rows, cols, result;
     int *r = &rows, *c = &cols;
@@ -26,34 +24,31 @@ int main(int argc, char* argv[]) {
        return 1;
     }
 
-    mymaze = read_maze(argv[1], r, c);// <---TASK: COMPLETE THIS FOR CHECKPOINT 1
+    mymaze = read_maze(argv[1], r, c);
 
-    if (mymaze == NULL) {
+    if (mymaze == NULL)
+    {
       cout << "Error, input format incorrect." << endl;
       return 1;
     }
 
-
-     //================================
-    // When working on Checkpoint 4, you will call maze_search here.
-    // But for Checkpoint 1, just assume we found the path.
-
-    result = maze_search(mymaze, rows, cols); // <--- TASK: CHANGE THIS FOR CHECKPOINT 4
+    result = maze_search(mymaze, rows, cols);
 
     // examine value returned by maze_search and print appropriate output
-    if (result == 1) { // path found!
+    if (result == 1)
+    {
       print_maze(mymaze, rows, cols);
     }
-    else if (result == 0) { // no path :(
+    else if (result == 0)
+    {
       cout << "No path could be found!" << endl;
     }
-    else { // result == -1
+    else
+    {
       cout << "Invalid maze." << endl;
     }
 
-     //================================
-    // ADD CODE BELOW
-    // to delete all memory that read_maze allocated: CHECKPOINT 2
+    // free up memory
     for(int i = 0; i < rows; i++)
     {
         delete[] mymaze[i];
@@ -63,24 +58,14 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-/**************************************************
- * Attempt to find shortest path and return:
- *  1 if successful
- *  0 if no path exists
- * -1 if invalid maze (not exactly one S and one F)
- *
- * If path is found fill it in with '*' characters
- *  but don't overwrite the 'S' and 'F' cells
- * NOTE: don't forget to deallocate memory in here too!
- *************************************************/
 
-// Things to consider
-// ending up at dead end in maze
-// how to backtrack from dead end
-// how to find optimal path
+// Finds the shortest path and returns:
+// 1 if successful
+// 0 if no path exists
+//-1 if invalid maze (not exactly one S and one F)
+// If path is found fills it in with '*' characters
 int maze_search(char** maze, int rows, int cols) 
 {
-    // *** You complete **** CHECKPOINT 4
     Queue q(rows*cols);
     Location next;
     Location current;
@@ -116,7 +101,7 @@ int maze_search(char** maze, int rows, int cols)
             }
         }
     }
-
+    // check for valid starting and end positions
     if(s != 1 || f != 1)
     {
         return -1;
@@ -124,7 +109,7 @@ int maze_search(char** maze, int rows, int cols)
 
     int exploredIndex = 1;
     int* size = &exploredIndex;
-    // once we find the finish cell the queue will keep poping until empty
+    // if no finish cell is found queue will keep removing from front until empty
     while(!q.is_empty())
     {
         current = q.remove_from_front();
@@ -132,13 +117,17 @@ int maze_search(char** maze, int rows, int cols)
         // west: -1 col
         // south: +1 row
         // east: +1 col
+
+        // north
         // when checking a position in the maze we must make sure it exists
         if(current.row - 1 >= 0)
         {
             next.row = current.row - 1;
             next.col = current.col;
+            // Don't want walls apart of the path
             if(maze[next.row][next.col] != '#')
             {
+                // false actually means it was added to explored
                 if(!addToExplored(next, size, explored))
                 {
                     q.add_to_back(next);
@@ -150,6 +139,7 @@ int maze_search(char** maze, int rows, int cols)
                 break;
             }
         }
+        // west
         if(current.col - 1 >= 0)
         {
             next.row = current.row;
@@ -167,6 +157,7 @@ int maze_search(char** maze, int rows, int cols)
                 break;
             }
         }
+        // south
         if(current.row + 1 < rows)
         {
             next.row = current.row + 1;
@@ -184,6 +175,7 @@ int maze_search(char** maze, int rows, int cols)
                 break;
             }
         }
+        // east
         if(current.col + 1 < cols)
         {
             next.row = current.row;
@@ -202,12 +194,14 @@ int maze_search(char** maze, int rows, int cols)
             }
         }
     }
+    // the last explored location should be the finish cell
     Location valid = explored[exploredIndex - 1];
     if(valid.row != finish.row || valid.col != finish.col)
     {
         return 0;
     }
     backtracking(predecessor, maze, finish, *size);
+    // free up memory
     delete[] explored;
     for(int i = 0; i < rows; i++)
     {
--- queue.cpp
+++ queue.cpp
@@ -1,43 +1,33 @@
-/* 
-queue.cpp
-*/
-
 #include "queue.h"
 
-//Constructor. maxlen must be as large as the total number
-// of Locations that will ever be entered into this Queue.
-// This is complete, you don't need to change it.
-Queue::Queue(int maxlen) {
+Queue::Queue(int maxlen)
+{
    contents = new Location[maxlen];
    head = 0;
    tail = 0;
 }
 
-//Destructor. releases resources. C++ will call it automatically.
-//This is complete, you don't need to change it.
-Queue::~Queue() {
+Queue::~Queue()
+{
   delete[] contents;
 }
 
-//Insert a new Location at the end/back of our list   
-void Queue::add_to_back(Location loc) {
-	
-   // *** You complete **** CHECKPOINT 3
+void Queue::add_to_back(Location loc)
+{
+
   contents[tail] = loc;
   tail++;
 }
 
-//Return and "remove" the oldest Location not already extracted
-Location Queue::remove_from_front() {
-    // *** You complete **** CHECKPOINT 3
+Location Queue::remove_from_front()
+{
     int temp = head;
     head++;
     return contents[temp];
 }
 
-//Is this Queue empty? (did we extract everything added?)
-//This is complete, you don't need to change it.
-bool Queue::is_empty() {
+bool Queue::is_empty()
+{
    return head == tail;
 }
 
